268909d454c6f4d9477d3694d365d46668d4134f
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-08-03 16:07:47 +0200

    add lambda code

---
+++ terraform/lambda-client.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/functions/test.js"
+  output_path = "${path.module}/functions/test.zip"
+}
+
+resource "aws_lambda_function" "test" {
+  filename      = "${path.module}/functions/test.zip"
+  function_name = "test"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "test.handler"
+  runtime = "nodejs18.x"
+}
+
+resource "aws_lambda_function_url" "public_url" {
+  function_name      = aws_lambda_function.test.function_name
+  authorization_type = "NONE"
+}

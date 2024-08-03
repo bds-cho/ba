522d4bc90242487a22e8dfcb56f5429a390f0a96
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-08-03 16:07:15 +0200

    update startup scripts

--- terraform/ec2-client.tf
+++ terraform/ec2-client.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2-client" {
   ami = "ami-0e872aee57663ae2d" # ubuntu-server-24.04-amd64-hvm
-  instance_type = "t2.micro"
+  instance_type = "t2.micro"    # 1CPU-1GiB
   tags = { Name = "ec2-client" }
+  availability_zone = var.az
 
   user_data = <<-EOL
     #!/bin/bash -xe
@@ -9,5 +10,6 @@ resource "aws_instance" "ec2-client" {
     apt install -y git curl zip jq
     wget https://github.com/grafana/k6/releases/download/v0.52.0/k6-v0.52.0-linux-amd64.deb -P /
     dpkg -i /k6-v0.51.0-linux-amd64.deb && rm -f /k6-v0.51.0-linux-amd64.deb
+    git clone https://github.com/bds-cho/ba.git /root/
   EOL
 }
--- terraform/helper-ec2.tf
+++ terraform/helper-ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "helper-ec2" {
   ami = "ami-0e872aee57663ae2d" # ubuntu-server-24.04-amd64-hvm
-  instance_type = "t2.micro"
+  instance_type = "t2.micro"    # 1CPU-1GiB
   tags = { Name = "helper-ec2" }
+  availability_zone = var.az
 
   user_data = <<-EOL
     #!/bin/bash -xe
@@ -9,5 +10,6 @@ resource "aws_instance" "helper-ec2" {
     apt install -y git curl zip jq
     wget https://github.com/grafana/k6/releases/download/v0.52.0/k6-v0.52.0-linux-amd64.deb -P /
     dpkg -i /k6-v0.51.0-linux-amd64.deb && rm -f /k6-v0.51.0-linux-amd64.deb
+    git clone https://github.com/bds-cho/ba.git /root/
   EOL
 }

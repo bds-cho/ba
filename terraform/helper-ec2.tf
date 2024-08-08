0c02ed5599ab9066c04fdac6c977a70d64535f04
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-08-08 20:04:32 +0200

    fix github PAT

--- terraform/helper-ec2.tf
+++ terraform/helper-ec2.tf
@@ -7,9 +7,7 @@ resource "aws_instance" "helper-ec2" {
   user_data = <<-EOL
     #!/bin/bash -xe
     apt update
-    apt install -y git curl zip jq
-    wget https://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@github.com/grafana/k6/releases/download/v0.52.0/k6-v0.52.0-linux-amd64.deb -P /
-    dpkg -i /k6-v0.52.0-linux-amd64.deb && rm -f /k6-v0.52.0-linux-amd64.deb
-    cd /root/ && git clone https://github.com/bds-cho/ba.git
+    apt install -y git
+    cd /root/ && git clone https://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@github.com/bds-cho/ba.git
   EOL
 }

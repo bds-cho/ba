17c5088bf895e0c437e81824b6322258de445579
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-08-11 14:11:09 +0200

    implement item generation

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -3,15 +3,21 @@ resource "aws_dynamodb_table" "dynamo" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
-  hash_key       = "column1"
-  range_key      = "column2"
+  hash_key       = "id"
   attribute {
-    name = "column1"
-    type = "S"
-  }
-  attribute {
-    name = "column2"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 }
 
+resource "aws_dynamodb_table_item" "items" {
+  count = 10
+  table_name = aws_dynamodb_table.dynamo.name
+  hash_key   = aws_dynamodb_table.dynamo.hash_key
+  item = <<ITEM
+  {
+    "id": {"N":"${count.index+1}"},
+    "email": {"S":"user${format("%03d",count.index+1)}@example.com"}
+  }
+  ITEM
+}

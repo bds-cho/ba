b337676a141e71e4cea28f84ac0bacf036603bda
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-08-12 17:45:36 +0200

    add vpc_config and policy

--- terraform/lambda-client.tf
+++ terraform/lambda-client.tf
@@ -14,18 +14,21 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "${path.module}/util/test.js"
-  output_path = "${path.module}/util/test.zip"
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_lambda_function" "test" {
-  filename      = "${path.module}/util/test.zip"
+  filename      = "${path.module}/util/lambda-rds-fn.zip"
   function_name = "test"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "test.handler"
   runtime       = "nodejs18.x"
+  vpc_config {
+    subnet_ids = [ "subnet-0658837f6a09e2595" ]
+    security_group_ids = [ "sg-06f602494cd511886" ]
+  }
 }
 
 resource "aws_lambda_function_url" "public_url" {
